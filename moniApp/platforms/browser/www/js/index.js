e9fa4b8b9938208cf83d346162c651da983143d7
Thaís Lescano <thaislescano@hotmail.com>
2018-08-10 15:01:26 -0300

    menu abre e fecha

--- moniApp/platforms/browser/www/js/index.js
+++ moniApp/platforms/browser/www/js/index.js
@@ -4,3 +4,17 @@
     window.location = path;
  }
 
+function mostrarMenu(){
+	/*alert('é hora do show');*/
+	var menu = document.getElementById("myMenu");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+        var icone = document.getElementById("more");
+        icone.innerHTML = "menu";
+    } else {
+        menu.style.display = "block";
+        var icone = document.getElementById("more");
+   		icone.innerHTML = "close";
+    }
+
+}
