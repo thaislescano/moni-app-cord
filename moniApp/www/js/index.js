5f157529048c59b80713df89ad254e857f23535e
Thaís Lescano <thaislescano@hotmail.com>
2018-08-16 13:58:12 -0300

    vida nova ao moni'

--- moniApp/www/js/index.js
+++ moniApp/www/js/index.js
@@ -1,32 +1,7 @@
-
-
-function chamarPagina(){
-	alert("clicado");
-	var xhr= new XMLHttpRequest();
-	xhr.open('GET', 'x.html', true);
-	xhr.onreadystatechange= function() {
-	    if (this.readyState!==4) return;
-	    if (this.status!==200) return; // or whatever error handling you want
-	    document.getElementById('btnFooter').innerHTML= this.responseText;
-	};
-	xhr.send();
-
-	//window.location = "pesquisar.html";
-}
-
-
-
-function mostrarMenu(){
-	/*alert('é hora do show');*/
-	var menu = document.getElementById("myMenu");
-    if (menu.style.display === "block") {
-        menu.style.display = "none";
-        var icone = document.getElementById("more");
-        icone.innerHTML = "menu";
-    } else {
-        menu.style.display = "block";
-        var icone = document.getElementById("more");
-   		icone.innerHTML = "close";
-    }
-
+function carregarView(pagina){
+	//nessa parte vai receber uma string com o nome da pagina que vc quer chamar e..
+	if(PAGINAS[pagina] !== undefined){
+		document.getElementById("corpo").innerHTML = PAGINAS[pagina];
+	}
+	//carregar ela dentro do #corpo se existir
 }
