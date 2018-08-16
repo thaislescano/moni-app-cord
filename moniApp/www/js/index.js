92c81c0e66e416badd91cbec8c5acc6c787ce5e3
Thaís Lescano <thaislescano@hotmail.com>
2018-08-16 19:24:06 -0300

    menu hamburguer

--- moniApp/www/js/index.js
+++ moniApp/www/js/index.js
@@ -5,3 +5,12 @@ function carregarView(pagina){
 	}
 	//carregar ela dentro do #corpo se existir
 }
+
+function mostrarMenu(){
+	var menu = document.getElementById("myMenu");
+    if (menu.style.display === "none") {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+}
