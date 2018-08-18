1cc10f522c01d836953ecea9a26d475c248e80f0
Thaís Lescano <thaislescano@hotmail.com>
2018-08-18 10:04:19 -0300

    cards pelo js v1

--- moniApp/www/js/index.js
+++ moniApp/www/js/index.js
@@ -19,12 +19,30 @@ function mostrarMenu(){
 }
 
 function mostrarCards(){
+	//apenas um modelo
 	//depois: receber input
 	//buscar prof daquele input(disciplina)
 	//mostrar os cards com a informação
+	//cored de corda aleatoria depois?
 
+	//card
 	var card = document.createElement("div");
 	card.classList.add("info-card");
+	card.classList.add("flexbox-row-around");
 	document.body.appendChild(card);
+
+	//foto
+	var infoCard = document.getElementsByClassName("info-card")[0];
+	var imagem = document.createElement("img");
+	imagem.src="img/danpix.png";
+	imagem.alt="avatar do monitor";
+	infoCard.appendChild(imagem);
+
+	//info
+	var divInfo = document.createElement("div");
+	divInfo.classList.add("info-monitor");
+	divInfo.innerHTML = "<h3 class='nome'> Fulano de Tal Tal</h3><h4 class='dias'> Segunda</h4><h4 class='horario'> 14:00 - 16:00 </h4>"
+	infoCard.appendChild(divInfo);
+
 
 }
